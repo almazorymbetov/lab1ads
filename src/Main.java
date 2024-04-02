15a85895680821bdca9f7cd2eae6b41ab6613bc7
Almaz Orymbetov <230585@astanait.edu.kz>
2024-04-02 17:01:12 +0500

    First problem I used a loop that compares the initial minimum element and the next element, and if it is less than the initial one, then it will be the minimum

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] array = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            array[i] = sc.nextInt();
+        }
+        int res = min(n, array);
+        System.out.println("Minimum is " + res);
+        sc.close();
+    }
+    public static int min(int n, int[] array){
+        int res = array[0];
+        for(int i = 1;i < n;i++)
+        {
+            if(array[i]<res)
+            {
+                res = array[i];
+            }
+        }
+        return res;
+    }
+}
