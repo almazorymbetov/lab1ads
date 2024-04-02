be0636aa14fb5fc3e330f53397d8cf3a41b70943
Almaz Orymbetov <230585@astanait.edu.kz>
2024-04-02 19:06:28 +0500

    Solved seventh problem

---
+++ src/problem7.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class problem7{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] array = new int[n];
+        for(int i = 0;i < n;i++){
+            array[i] = sc.nextInt();
+        } //input elements
+        revers(array,0,n-1); //called recursive function
+        for(int i = 0;i < n;i++){
+            System.out.println(array[i]); //print revesred array
+        }
+    }
+    public static void revers(int[] array,int index1, int indexl){
+        if(index1>=indexl){
+            return; //it used when the number of elements equal to 1
+        }
+        int index0 = array[index1]; //this part is responsible to swap first and last elements of array
+        array[index1] = array[indexl];
+        array[indexl] = index0;
+        revers(array, index1+1,indexl-1); //finally, it also calls recursive function
+        /*
+        index1 +1 means that first index moves toward to the last, while last to the first, and
+        they moving to the "center" of array
+         */
+    }
+}
